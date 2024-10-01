f91b59efda5a8ed2641c368a2c0f60b59425fde8
hmdat294 <hodat2092004@gmail.com>
2024-10-01 16:54:56 +0700

    minhdat update

--- fe/src/app/home/right-home/right-home.component.ts
+++ fe/src/app/home/right-home/right-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,6 +8,28 @@ import { RouterModule } from '@angular/router';
   templateUrl: './right-home.component.html',
   styleUrl: './right-home.component.css'
 })
-export class RightHomeComponent {
+export class RightHomeComponent implements OnInit {
 
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+
+    this.accordion();
+
+  }
+
+  accordion() {
+    const headers = this.el.nativeElement.querySelectorAll('.a-accordion-header') as NodeListOf<HTMLElement>;
+
+    headers.forEach((header: HTMLElement) => {
+      this.renderer.listen(header, 'click', () => {
+        const panel = header.nextElementSibling as HTMLElement;
+
+        header.classList.toggle('active');
+        panel.classList.toggle('open');
+
+        panel.style.maxHeight = (panel.classList.contains('open')) ? `${panel.scrollHeight}px` : '0px';
+      });
+    });
+  }
 }
