43081023a515c6d36230b34da6fe3dba1ebcab3e
Chick-kun <ThanhCuong0794294868@gmail.com>
2024-10-08 15:22:58 +0700

    add comment

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -11,11 +11,16 @@ use Illuminate\Validation\Rules;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use App\Models\User as UserModel;
-
-use function Pest\Laravel\json;
+use App\Models\Permission as PermissionModel;
 
 class AuthController extends Controller
 {
+
+    /**
+     * - 422: Thông tin đăng nhập không hợp lệ
+     * - 403: Người dùng không có quyền đăng nhập
+     * - 200: Đăng nhập thành công
+     */
     public function login(Request $request)
     {
         $request->validate([
@@ -28,8 +33,14 @@ class AuthController extends Controller
                 'message' => 'Incorrect login information.',
             ], 422);
         }
-    
+
         $user = UserModel::find(Auth::user()->id);
+        // check if user has permission to login
+        if (!$user->permissions->contains('name', 'login')) {
+            return response()->json([
+                'message' => 'User does not have permission to login.',
+            ], 403);
+        }
         event(new UserLoggedInEvent($user));
     
         return response()->json([
@@ -39,7 +50,10 @@ class AuthController extends Controller
     }
 
 
-    
+    /**
+     * - 422: Người dùng đã tồn tại
+     * - 200: Đăng ký thành công
+     */
     public function register(Request $request)
     {
         try {
@@ -65,6 +79,8 @@ class AuthController extends Controller
             'last_activity' => now(),
         ]);
 
+        $user->permissions()->attach(PermissionModel::all()->pluck('id')->toArray());
+
         event(new UserRegisteredEvent($user));
 
         return response()->json([
@@ -73,6 +89,9 @@ class AuthController extends Controller
         ], 200);
     }
 
+    /**
+     * - 200: Đăng xuất thành công
+     */
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
--- server/app/Http/Controllers/VerificationCodeController.php
+++ server/app/Http/Controllers/VerificationCodeController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\VerificationCodeModel;
+use App\Models\VerificationCode;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\VerificationCodeNotification;
 
 class VerificationCodeController extends Controller
 {
+    /**
+     * - 400: Thông tin liên hệ không hợp lệ
+     * - 400: Mã xác thực không hợp lệ
+     * - 200: Mã xác thực chính xác
+     */
     public function verify(Request $request)
     {
         $request->validate([
@@ -31,15 +36,17 @@ class VerificationCodeController extends Controller
             ], 400);
         }
 
-        $verifyCode = (new VerificationCodeModel)->verifyCode($request->code, $email, $phone_number);
+        $verifyCode = (new VerificationCode)->verifyCode($request->code, $email, $phone_number);
 
         return response()->json([
             'message' => $verifyCode ? 'Verification code is correct' : 'Verification code is incorrect',
-        ]);
-
-
+        ], $verifyCode ? 200 : 400);
     }
 
+    /**
+     * - 400: Thông tin liên hệ không hợp lệ
+     * - 200: Mã xác thực đã được gửi đến thông tin liên hệ
+     */
     public function create(Request $request)
     {
         $request->validate([
@@ -61,14 +68,14 @@ class VerificationCodeController extends Controller
             ], 400);
         }
     
-        $code = (new VerificationCodeModel)->generateVerificationCode($email, $phone_number);
+        $code = (new VerificationCode)->generateVerificationCode($email, $phone_number);
         if ($email) {
             Mail::to($email)->queue(new VerificationCodeNotification($code));
         }
     
         return response()->json([
             'message' => 'Verification code has been sent to your contact info',
-        ]);
+        ], 200);
     }
     
 }
