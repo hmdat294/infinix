43944f5a86675ad0589ccca6db011f19754cba6e
HThais72 <hongthai7204@gmail.com>
2024-10-10 23:36:26 +0700

    thai push

--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainHomeComponent } from './home/main-home/main-home.component';
 import { MainProfileComponent } from './profile/main-profile/main-profile.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 
 export const routes: Routes = [
     {path: '', component: MainHomeComponent},
-    {path: 'profile', component: MainProfileComponent}
+    {path: 'profile', component: MainProfileComponent},
+    {path: 'admindashboard', component: AdminDashboardComponent}
 ];
