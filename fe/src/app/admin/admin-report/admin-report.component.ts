df2fbab476c46f31c9bd8f4e1d5cce9ae91f6d25
bin0931142237 <bin0931142237@gmail.com>
2024-10-10 19:48:00 +0700

    thienbui

---
+++ fe/src/app/admin/admin-report/admin-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-report.component.html',
+  styleUrl: './admin-report.component.css'
+})
+export class AdminReportComponent {
+
+}
