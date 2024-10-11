ef0a49cf96b8b02daaa5e2abbb8f3091cbbecf8e
bin0931142237 <bin0931142237@gmail.com>
2024-10-11 19:27:48 +0700

    thienbui

--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { MainHomeComponent } from './home/main-home/main-home.component';
 import { MainProfileComponent } from './profile/main-profile/main-profile.component';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 
+
 export const routes: Routes = [
     {path: '', component: MainHomeComponent},
-    {path: 'profile', component: MainProfileComponent},
-    {path: 'admindashboard', component: AdminDashboardComponent}
+    {path: 'profile', component: MainProfileComponent}
 ];
