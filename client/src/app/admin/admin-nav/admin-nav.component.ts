206141ed475909f780ebd1413963b6670786e4d3
bin0931142237 <bin0931142237@gmail.com>
2024-10-15 02:37:37 +0700

    thienbui

--- client/src/app/admin/admin-nav/admin-nav.component.ts
+++ client/src/app/admin/admin-nav/admin-nav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './admin-nav.component.html',
   styleUrl: './admin-nav.component.css'
 })
