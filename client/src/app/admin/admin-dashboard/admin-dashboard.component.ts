275b08db04885e5a711cdbf423972453fdf9e00f
HThais72 <hongthai7204@gmail.com>
2024-10-16 01:44:33 +0700

    "da xong 2p"

--- client/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ client/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import ApexCharts, { ApexOptions } from 'apexcharts';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,6 +8,152 @@ import { Component } from '@angular/core';
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit(): void {
+    this.renderChart();
+    this.renderDonutChart();
+    this.renderRadialBarChart();
+    this.renderLineChart1();
+  }
+
+  renderChart(): void {
+    const chartOptions: ApexOptions = {
+      chart: {
+        foreColor: '#9ba7b2',
+        height: 460,
+        type: 'line',
+        zoom: { enabled: false },
+        dropShadow: { enabled: true, top: 3, left: 2, blur: 4, opacity: 0.1 }
+      },
+      stroke: { width: 5, curve: 'smooth' },
+      colors: ["#5283FF", '#F1C40F', '#FF4C92', "#17a00e"],
+      series: [{
+        name: "Users",
+        data: [55, 74, 64, 84, 54, 92]
+      }, {
+        name: "Post",
+        data: [54, 55, 55, 54, 54, 54]
+      }, {
+        name: "React",
+        data: [83, 84, 74, 92, 73, 73]
+      }, {
+        name: "Total",
+        data: [100, 110, 90, 120, 90, 100]
+      }],
+      xaxis: {
+        type: 'datetime',
+        categories: ['1/11/2000', '2/11/2000', '3/11/2000', '4/11/2000', '5/11/2000', '6/11/2000'],
+      },
+      title: {
+        text: 'Growth statistics',
+        offsetY: 0,
+        offsetX: 20
+      },
+      markers: {
+        size: 5,
+        strokeColors: "#fff",
+        strokeWidth: 1,
+        hover: {
+          size: 7
+        }
+      },
+      yaxis: {
+        title: {
+          text: 'Engagement',
+        },
+      }
+    };
+
+    const chart = new ApexCharts(document.querySelector('#chart2'), chartOptions);
+    chart.render();
+  }
+
+  renderDonutChart(): void {
+    const donutChartOptions: ApexOptions = {
+      series: [25, 25, 25, 25],
+      chart: {
+        foreColor: '#9ba7b2',
+        height: 240,
+        type: 'donut',
+        dropShadow: { enabled: true, top: 3, left: 2, blur: 4, opacity: 0.1 }
+      },
+      colors: ["#ffc107", "#0d6efd", "#17a00e", "#f41127"],
+      labels: ['User report', 'Comment report', 'Post report', 'Group report']
+    };
+
+    const donutChart = new ApexCharts(document.querySelector('#chart9'), donutChartOptions);
+    donutChart.render();
+  }
+
+
+
+  renderRadialBarChart(): void {
+    const radialBarOptions: ApexOptions = {
+      series: [44, 55, 67, 83],
+      chart: {
+        height: 350,
+        type: 'radialBar',
+        dropShadow: { enabled: true, top: 3, left: 2, blur: 4, opacity: 0.1 }
+      },
+      plotOptions: {
+        radialBar: {
+          dataLabels: {
+            total: {
+              show: true,
+              label: 'Tổng quan',
+            }
+          }
+        }
+      },
+      colors: ["#17a00e", "#f41127", "#0d6efd", "#ffc107"],
+      labels: ['User report', 'Comment report', 'Post report', 'Post report']
+    };
+
+    const radialBarChart = new ApexCharts(document.querySelector('#chart13'), radialBarOptions);
+    radialBarChart.render();
+  }
+
+
+  renderLineChart1(): void {
+    const lineChart1Options: ApexOptions = {
+      chart: {
+        height: 460,
+        type: 'line',
+        zoom: { enabled: false },
+        dropShadow: { enabled: true, top: 3, left: 2, blur: 4, opacity: 0.1 }
+      },
+      stroke: { width: 5, curve: 'smooth' },
+      colors: ["#f41127"],
+      series: [{
+        name: 'Likes',
+        data: [14, 3, 20, 9, 29]
+      }],
+      xaxis: {
+        type: 'datetime',
+        categories: ['1/11/2000', '2/11/2000', '3/11/2000', '4/11/2000', '5/11/2000'],
+      },
+      title: {
+        text: 'Line Chart'
+      },
+      markers: {
+        size: 5,
+        colors: ["#f41127"],
+        strokeColors: "#fff",
+        strokeWidth: 2,
+        hover: {
+          size: 7,
+        }
+      },
+      yaxis: {
+        title: {
+          text: 'Engagement',
+        },
+      }
+    };
+
+    const lineChart1 = new ApexCharts(document.querySelector('#chart1'), lineChart1Options);
+    lineChart1.render();
+  }
 }
