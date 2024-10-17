c6344124723e28b302eab681b7c3d2bf615af7b7
bin0931142237 <bin0931142237@gmail.com>
2024-10-17 20:18:12 +0700

    aaaa

---
+++ client/src/app/admin/admin-content/admin-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-content.component.html',
+  styleUrl: './admin-content.component.css'
+})
+export class AdminContentComponent {
+
+}
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -11,6 +11,7 @@ import { AdminMainUserComponent } from './admin/admin-main-user/admin-main-user.
 import { AdminMainReportComponent } from './admin/admin-main-report/admin-main-report.component';
 import { AdminMainEventComponent } from './admin/admin-main-event/admin-main-event.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AdminMainContentComponent } from './admin/admin-main-content/admin-main-content.component';
 
 export const routes: Routes = [
     { path: '', component: MainHomeComponent, canActivate: [AuthGuard] },
@@ -24,4 +25,5 @@ export const routes: Routes = [
     {path: 'admin/user', component: AdminMainUserComponent},
     {path: 'admin/report', component: AdminMainReportComponent},
     {path: 'admin/event', component: AdminMainEventComponent},
+    {path: 'admin/content', component: AdminMainContentComponent},
 ];
