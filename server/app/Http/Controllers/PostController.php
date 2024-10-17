e44d5aa49fa189cd3e6f801eed3d77a72b840046
hmdat294 <hodat2092004@gmail.com>
2024-10-17 23:16:03 +0700

    event post

--- server/app/Http/Controllers/PostController.php
+++ server/app/Http/Controllers/PostController.php
@@ -85,13 +85,12 @@ class PostController extends Controller
                     'path' => asset('storage/' . $media_path),
                     'type' => $media_type,
                 ]);
-
             }
         }
 
-        broadcast(new UserPostEvent($post->user_id, $post->id, $post->content))->toOthers();
+        event(new UserPostEvent($post->user_id, $post->id, $post->content));
 
-        $this->sendNotification($post);
+        // $this->sendNotification($post);
 
         return new PostResource($post);
     }
