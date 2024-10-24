affe682b176b0f1faed677acae16eba0f26b9e3f
Chick-kun <ThanhCuong0794294868@gmail.com>
2024-10-24 19:12:35 +0700

    #

--- server/app/Http/Resources/UserResource.php
+++ server/app/Http/Resources/UserResource.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Resources;
 
+use App\Models\FriendRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Log;
+use App\Models\FriendRequest as FriendRequestModel;
 
 class UserResource extends JsonResource
 {
@@ -22,6 +24,9 @@ class UserResource extends JsonResource
         $followers = collect();
         $followings = collect();
 
+        
+        $can_send_friend_request = null;
+
         if ($request->user()) {
             $friendsOf =  $request->user()->friendsOf ?: collect();
             $friendsOfMine =$request->user()->friendsOfMine ?: collect();
@@ -29,6 +34,15 @@ class UserResource extends JsonResource
             
             $followers = $this->followers;
             $followings = $this->followings;
+
+            if($friends->contains($this->id)) {
+                $can_send_friend_request = false;
+            } else {
+                $pendingFriendRequests = $request->user()->friendRequest()->where('status', 'pending')->pluck('receiver_id');
+                $can_send_friend_request = !$pendingFriendRequests->contains($this->id);
+            }
+
+
         }
 
 
@@ -48,6 +62,7 @@ class UserResource extends JsonResource
             'profile' => new ProfileResource($this->profile),
             'permissions' => $this->permissions,
             'is_friend' => $friends->contains($this->id),
+            'can_send_friend_request' => $can_send_friend_request
         ];
     }
 }
