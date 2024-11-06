7d40299ad1a2d27dc71b5bfd80720ca4721c4360
Chick-kun <ThanhCuong0794294868@gmail.com>
2024-11-06 20:32:37 +0700

    #

--- server/app/Http/Resources/NotificationResource.php
+++ server/app/Http/Resources/NotificationResource.php
@@ -16,13 +16,13 @@ class NotificationResource extends JsonResource
     {
         $data = parent::toArray($request);
         $data['user'] = new UserResource($this->user);
-        $data['target_user'] = new UserResource($this->targetUser);
-        $data['message'] = new MessageResource($this->message);
-        $data['post'] = new PostResource($this->post);
-        $data['comment'] = new CommentResource($this->comment);
-        $data['conversation'] = new ConversationResource($this->conversation);
-        $data['friend_request'] = new FriendRequestResource($this->friendRequest);
-        $data['conversation_invitation'] = new ConversationInvitationResource($this->conversationInvitation);
+        $data['target_user'] = new UserResource($this->targetUser) ?? collect();
+        $data['message'] = new MessageResource($this->message) ?? collect();
+        $data['post'] = new PostResource($this->post) ?? collect();
+        $data['comment'] = new CommentResource($this->comment) ?? collect();
+        $data['conversation'] = new ConversationResource($this->conversation) ?? collect();
+        $data['friend_request'] = new FriendRequestResource($this->friendRequest) ?? collect();
+        $data['conversation_invitation'] = new ConversationInvitationResource($this->conversationInvitation) ?? collect();
         $data['created_at'] = $this->created_at->format('Y-m-d H:i:s');
         $data['created_at_time'] = $this->created_at->format('H:i');
         $data['created_at_date'] = $this->created_at->format('Y-m-d');
