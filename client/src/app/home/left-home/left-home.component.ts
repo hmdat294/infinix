4db7e30959da530cb5cc070eb6681e4bd3225833
Chick-kun <ThanhCuong0794294868@gmail.com>
2024-11-09 20:26:53 +0700

    @

--- client/src/app/home/left-home/left-home.component.ts
+++ client/src/app/home/left-home/left-home.component.ts
@@ -41,7 +41,11 @@ export class LeftHomeComponent implements OnInit {
         this.eventService.bindEvent('App\\Events\\FriendRequestEvent', (data: any) => {
           console.log('Friend request event:', data);
 
-          if (data.status == "pending") this.userRequest.push(data);
+          if (data.status == "pending") {
+            if (data.sender_id != this.user.id) {
+              this.userRequest.push(data);
+            }
+          }
 
           if (data.status == "accepted") {
             this.userRequest = this.userRequest.filter((request: any) => request.id !== data.id);
