5e00a8d710fcf50adf8b30b461938e8449ffe6f5
Chick-kun <ThanhCuong0794294868@gmail.com>
2024-11-12 20:10:26 +0700

    #

--- server/app/Models/PostTag.php
+++ server/app/Models/PostTag.php
@@ -11,5 +11,14 @@ class PostTag extends Model
 
     protected $table = 'post_tags';
 
+    protected $fillable = [
+        'post_id',
+        'tag',
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
     
 }
