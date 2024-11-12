8a5575681ede0f3d80ff73970797eafa45c163c0
hmdat294 <hodat2092004@gmail.com>
2024-11-12 23:14:18 +0700

    *

--- client/src/app/home/right-home/right-home.component.ts
+++ client/src/app/home/right-home/right-home.component.ts
@@ -20,6 +20,9 @@ export class RightHomeComponent implements OnInit, AfterViewInit {
   showFriendMore: boolean = false;
   keyword: string = '';
   friendsSearch: any = [];
+  listUser: any;
+  listGroup: any;
+  conversation: any[] = [];
 
   constructor(
     private el: ElementRef,
@@ -73,9 +76,37 @@ export class RightHomeComponent implements OnInit, AfterViewInit {
 
 
               });
+
+            this.chatService.getListChat().subscribe(
+              (response) => {
+                this.listUser = response.data.filter((item: any) => item.is_group == 0);
+                this.listGroup = response.data.filter((item: any) => item.is_group == 1);
+                console.log(this.listUser);
+                console.log(this.listGroup);
+              });
           });
       }
     });
+
+    this.chatService.conversation$.subscribe(conversation => {
+      // console.log('Updated conversation from localStorage:', conversation);
+      this.conversation = conversation;
+    });
+  }
+
+  createChat(conversation_id: number) {
+    if (this.conversation.includes(conversation_id)) {
+      this.conversation = this.conversation.filter(id => id !== conversation_id);
+    }
+
+    if (this.conversation.length >= 5) {
+      this.conversation.shift();
+    }
+
+    this.conversation.push(conversation_id);
+
+    this.chatService.updateConversation(this.conversation);
+    this.chatService.tagOpenBoxChat = true;
   }
 
   searchFriend() {
