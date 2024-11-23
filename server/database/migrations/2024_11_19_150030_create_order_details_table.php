0061791a2cbed73f50ccda292bd3b7838ea3f83d
Chick-kun <ThanhCuong0794294868@gmail.com>
2024-11-23 19:36:07 +0700

    #

--- server/database/migrations/2024_11_19_150030_create_order_details_table.php
+++ server/database/migrations/2024_11_19_150030_create_order_details_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->integer('quantity');
             $table->integer('price');
-            $table->integer('total');
 
             $table->timestamps();
         });
