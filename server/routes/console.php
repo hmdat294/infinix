9153c87387336cb820c33a586cc54d177c6020c9
Chick-kun <ThanhCuong0794294868@gmail.com>
2024-12-20 01:46:37 +0700

    cancel order group

--- server/routes/console.php
+++ server/routes/console.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 use App\Models\Order;
+use App\Models\OrderGroup;
 use App\Models\UserPermission;
 
 // Artisan::command('inspire', function () {
@@ -12,8 +13,13 @@ use App\Models\UserPermission;
 // })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::call(function () {
-    Order::where('status', 'paid')->where('admin_paid', false)->where('created_at', '<', now()->subDays(7))
-    ->update(['admin_paid' => true]);
+    // Order::where('status', 'paid')->where('admin_paid', false)->where('created_at', '<', now()->subDays(7))
+    // ->update(['admin_paid' => true]);
+    $order_group = OrderGroup::where('payment_status', 'paid')->where('created_at', '<', now()->subDays(7))->get();
+    foreach ($order_group as $order) {
+        $order->admin_paid = true;
+        $order->save();
+    }
 
     UserPermission::where('enable_at', '<', now())->update(['is_active' => true, 'enable_at' => null]);
 })->daily();
